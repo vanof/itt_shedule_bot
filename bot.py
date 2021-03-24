502412e9147db46bccd909ddcc2be40edc5b7d54
vanof <58184758+vanof@users.noreply.github.com>
2021-03-24 17:27:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
 bot = telebot.TeleBot('1758490944:AAEzDcrkuilDBulhlNOzwb3UboeveZ9qw-0')
 
-@bot.message_handler(commands=['start', 'help'])
+bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
 
-@bot.message_handler(content_types=['text'])
+bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Привет!')
